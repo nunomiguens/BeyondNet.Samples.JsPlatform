ba22f319811c3c8b9615ca764c78506e04b253e0
BeyondNet <alberto.arroyo@tranzact.net>
2021-08-02 08:32:13 -0500

    Uploading condfig modules and partial registrations with nest

--- frameworks/nest/official-course/iluvcoffe/src/app.module.ts
+++ frameworks/nest/official-course/iluvcoffe/src/app.module.ts
@@ -9,18 +9,20 @@ import { ConfigModule } from '@nestjs/config';
 
 @Module({
   imports: [
+    TypeOrmModule.forRootAsync({
+      useFactory: () => ({
+        type: 'postgres',
+        host: process.env.DATABASE_HOST,
+        port: parseInt(process.env.DATABASE_PORT) || 5432,
+        username: process.env.DATABASE_USER,
+        password: process.env.DATABASE_PASSWORD,
+        database: process.env.DATABASE_NAME,
+        autoLoadEntities: true,
+        synchronize: true, // disabled in production
+      }),
+    }),
     ConfigModule.forRoot(),
     CoffeesModule,
-    TypeOrmModule.forRoot({
-      type: 'postgres',
-      host: process.env.DATABASE_HOST,
-      port: parseInt(process.env.DATABASE_PORT) || 5432,
-      username: process.env.DATABASE_USER,
-      password: process.env.DATABASE_PASSWORD,
-      database: process.env.DATABASE_NAME,
-      autoLoadEntities: true,
-      synchronize: true, // disabled in production
-    }),
     CoffeeRatingModule,
     // DatabaseModule,
   ],
